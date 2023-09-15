cfceb6b807fbc659a45319b89b036d15969f00a2
29deepanshutyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2023-09-15 21:52:30 +0530

    contact added in admin

--- Django/Hello/home/models.py
+++ Django/Hello/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField( max_length=122)
+    email=models.CharField(max_length=122)
+    message=models.CharField(max_length=1222)
+    def __str__(self) -> str:
+        return self.name
