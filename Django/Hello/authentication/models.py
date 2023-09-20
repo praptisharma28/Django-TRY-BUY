99646f3317b30d59c3f29b48fe80f375fba7f130
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-09-20 23:38:03 +0530

    edit

--- Django/Hello/authentication/models.py
+++ Django/Hello/authentication/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Login(models.Model):
     Username=models.CharField( max_length=122)
     Password=models.CharField(max_length=122)
--- Django/Hello/home/views.py
+++ Django/Hello/home/views.py
@@ -21,6 +21,21 @@ def contact(request):
         
     return render(request,'contact.html')
 def login(request):
-    return render(request ,'login.html')
+    if request.method=="POST":
+        print(request)
+        Username = request.POST['Username']
+        Password = request.POST['Password']
+        print(Username, Password)
+        login=Login(Username=Username,Password=Password)
+        login.save()
+    return render(request,'login.html')
 def signup(request):
+    if request.method=="POST":
+        print(request)
+        Username = request.POST['Username']
+        Password = request.POST['Password']
+        Confirm_Password = request.POST['Confirm_Password']
+        print(Username, Password)
+        signup=Signup(Username=Username,Password=Password,Confirm_Password=Confirm_Password)
+        signup.save()
     return render(request,'signup.html')
