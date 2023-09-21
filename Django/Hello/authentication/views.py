9477abbfad69a996a8f780d5e153fea340eb7811
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-09-21 23:48:29 +0530

    edit

--- Django/Hello/authentication/views.py
+++ Django/Hello/authentication/views.py
@@ -1,34 +1,9 @@
 from django.shortcuts import render, redirect
 
-# authentication/views.py
+
 from django.contrib.auth import login, authenticate
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
-from authentication.models import Login, Signup
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = CustomAuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('home')  # Change 'home' to your desired URL
-#     else:
-#         form = CustomAuthenticationForm()
-#     return render(request, 'login.html', {'form': form})
-
-# def signup_view(request):
-#     if request.method == 'POST':
-#         form = CustomUserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('home')  # Change 'home' to your desired URL
-#     else:
-#         form = CustomUserCreationForm()
-#     return render(request, 'signup.html', {'form': form})
-
+f
 def index(request):
     return render(request,'index.html')
 def about(request):
