c16e238b14e76c9220ecc76d6c84495dffdaf7ea
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-09-21 23:57:05 +0530

    ..

--- Django/Hello/home/views.py
+++ Django/Hello/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from home.models import Contact
-# Create your views here.
+
 # def index(request):
 #     return HttpResponse("This is homepage")
 
