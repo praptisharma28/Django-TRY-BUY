88b88f74ddb33559cea473e26dec2221afc22b39
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-09-23 22:46:30 +0530

    commits

--- Django/Hello/home/views.py
+++ Django/Hello/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from home.models import Contact
-from home.models import Login,Signup
+# from home.models import Login,Signup
 # def index(request):
 #     return HttpResponse("This is homepage")
 
@@ -21,21 +21,21 @@ def contact(request):
         
     return render(request,'contact.html')
 def login(request):
-    if request.method=="POST":
-        print(request)
-        Username = request.POST['Username']
-        Password = request.POST['Password']
-        print(Username, Password)
-        login=Login(Username=Username,Password=Password)
-        login.save()
+    # if request.method=="POST":
+    #     print(request)
+    #     Username = request.POST['Username']
+    #     Password = request.POST['Password']
+    #     print(Username, Password)
+    #     login=Login(Username=Username,Password=Password)
+    #     login.save()
     return render(request,'login.html')
 def signup(request):
-    if request.method=="POST":
-        print(request)
-        Username = request.POST['Username']
-        Password = request.POST['Password']
-        Confirm_Password = request.POST['Confirm_Password']
-        print(Username, Password)
-        signup=Signup(Username=Username,Password=Password,Confirm_Password=Confirm_Password)
-        signup.save()
+    # if request.method=="POST":
+    #     print(request)
+    #     Username = request.POST['Username']
+    #     Password = request.POST['Password']
+    #     Confirm_Password = request.POST['Confirm_Password']
+    #     print(Username, Password)
+    #     signup=Signup(Username=Username,Password=Password,Confirm_Password=Confirm_Password)
+    #     signup.save()
     return render(request,'signup.html')
