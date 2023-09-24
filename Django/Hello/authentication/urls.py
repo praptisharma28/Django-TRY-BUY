8cbac861b99aed9f9e4e9aa1cf4b0916fc8638a9
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-09-24 23:33:04 +0530

    new

--- Django/Hello/authentication/urls.py
+++ Django/Hello/authentication/urls.py
@@ -1,25 +1,15 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from . import views
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include('authentication.urls')),
-#     path('', views.index),
-#     path('contact/', views.contact, name='contact'),
-#     path('about/', views.about, name='about'),
-#     path('login/', views.login, name='login'),
-#     path('signup/', views.signup, name='signup'),
-
-# ]
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),
+    path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
+    path('', views.index),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+
 ]
+
