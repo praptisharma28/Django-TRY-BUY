1f8e649d555c1abaa6d0e6cdd97ebe1a02a662a3
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-09-26 21:27:43 +0530

    ,,

--- Django/Hello/home/admin.py
+++ Django/Hello/home/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
 from home.models import Contact
 from authentication.models import Login,Signup
 # Register your models here.
 admin.site.register(Contact)
-# admin.site.register(Login)
-# admin.site.register(Signup)
+
 
 
 
